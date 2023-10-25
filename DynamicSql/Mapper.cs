558ab055d386da93ced85c4f2a890cf97b7608ec
baozhangchi <baozhangchi@live.com>
2023-10-25 14:36:36 +0800

    添加sqlmap

---
+++ DynamicSql/Mapper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace DynamicSql
+{
+    public class Mapper: ISqlMapper
+    {
+
+    }
+
+    public interface ISqlMapper
+    {
+        int Insert(string statementName, object item);
+        int Update(string statementName, object item);
+        int Delete(string statementName, object primaryKey);
+        T QueryForObject<T>(string statementName, object primaryKey);
+        IList<T> QueryForList<T>(string statementName, object parameterObject);
+    }
+}
---
+++ DynamicSql/SqlMap.cs
@@ -0,0 +1,119 @@
+﻿#region
+
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+#endregion
+
+namespace DynamicSql
+{
+    [XmlRoot("sqlMap")]
+    public class SqlMap
+    {
+        [XmlArray("alias")]
+        [XmlArrayItem("alias")]
+        public List<TypeAlias> Alias { get; set; } = new List<TypeAlias>();
+
+        [XmlArray("resultMaps")]
+        [XmlArrayItem("resultMap")]
+        public List<ResultMap> ResultMaps { get; set; } = new List<ResultMap>();
+
+        [XmlElement("statements")]
+        public Statements Statements { get; set; } = new Statements();
+    }
+
+    public class Statements
+    {
+        [XmlElement("select")]
+        public List<SelectStatement> SelectStatements { get; set; } = new List<SelectStatement>();
+        [XmlElement("insert")]
+        public List<InsertStatement> InsertStatements { get; set; } = new List<InsertStatement>();
+        [XmlElement("update")]
+        public List<UpdateStatement> UpdateStatements { get; set; } = new List<UpdateStatement>();
+        [XmlElement("delete")]
+        public List<DeleteStatement> DeleteStatements { get; set; } = new List<DeleteStatement>();
+    }
+
+    public abstract class Statement
+    {
+        [XmlAttribute("id")]
+        public string Id { get; set; }
+
+        [XmlAttribute("resultMap")]
+        public string ResultMap { get; set; }
+
+        [XmlAttribute("parameterClass")]
+        public string ParameterClass { get; set; }
+
+        [XmlAttribute("extends")]
+        public string Extends { get; set; }
+
+        [XmlElement("selectKey")]
+        public List<SelectKey> SelectKeys { get; set; } = new List<SelectKey>();
+
+        [XmlText]
+        public string Content { get; set; }
+    }
+
+    public class SelectKey
+    {
+        [XmlAttribute("property")]
+        public string Property { get; set; }
+
+        [XmlAttribute("type")]
+        public string Type { get; set; }
+
+        [XmlAttribute("resultClass")]
+        public string ResultClass { get; set; }
+
+        [XmlText]
+        public string Content { get; set; }
+    }
+
+    public class DeleteStatement : Statement
+    {
+    }
+
+    public class UpdateStatement : Statement
+    {
+    }
+
+    public class InsertStatement : Statement
+    {
+    }
+
+    public class SelectStatement : Statement
+    {
+    }
+
+    public class ResultMap
+    {
+        [XmlAttribute("id")]
+        public string Id { get; set; }
+
+        [XmlAttribute("class")]
+        public string Class { get; set; }
+
+
+        [XmlElement("result")]
+        public List<ResultItem> ResultItems { get; set; } = new List<ResultItem>();
+    }
+
+    public class ResultItem
+    {
+        [XmlAttribute("property")]
+        public string Property { get; set; }
+
+        [XmlAttribute("column")]
+        public string Column { get; set; }
+    }
+
+    public class TypeAlias
+    {
+        [XmlAttribute("alias")]
+        public string Alias { get; set; }
+
+        [XmlAttribute("type")]
+        public string Type { get; set; }
+    }
+}
